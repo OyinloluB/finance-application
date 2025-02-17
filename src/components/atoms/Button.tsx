239d042b3e2cc85c81aa3c1a544bbd025d9fb9cf
OyinloluB <oyinloluwab@gmail.com>
2025-02-17 14:25:51 -0500

    updated tertiary button

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CaretRightIcon from "./icons/CaretRightIcon";
 
 type ButtonType = "primary" | "secondary" | "tertiary" | "destroy";
 
@@ -18,15 +19,20 @@ const Button = ({ text = "Placeholder", type, className }: ButtonProps) => {
     primary: "bg-grey-900 text-white hover:bg-grey-500",
     secondary:
       "bg-beige-100 text-grey-900 hover:bg-white border border-transparent hover:border-beige-500",
-    tertiary: "text-grey-500 flex align-center gap-150 hover:text-grey-900",
+    tertiary: "text-grey-500 flex items-center gap-100 hover:text-grey-900",
     destroy:
       "relative bg-secondary-red text-white overflow-hidden " +
       "after:content-[''] after:absolute after:inset-0 after:bg-white after:opacity-0 after:transition-opacity after:duration-200 hover:after:opacity-20",
   };
 
+  console.log(type);
+
   return (
     <div className={`${baseStyles} ${buttonStyles[type]} ${className ?? ""}`}>
       {text}
+      {type === "tertiary" && (
+        <CaretRightIcon className="w-4 h-4 transition-colors duration-200 text-inherit" />
+      )}
     </div>
   );
 };
