d228e87f6b210d71d8c1c0d7f528dae078246519
OyinloluB <oyinloluwab@gmail.com>
2025-03-09 23:47:07 -0400

    setting up bills

---
+++ src/app/dashboard/recurring-bills/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Bills = () => {
+  return (
+    <div className="flex-1 p-400 min-h-screen">
+      <div className="mb-400">
+        <h1 className="text-preset-1 font-bold text-grey-900">
+          Recurring Bills
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Bills;
