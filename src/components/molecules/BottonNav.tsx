db0f79323c57b994b5ece16398b0e1e058ee9f4d
OyinloluB <oyinloluwab@gmail.com>
2025-03-31 21:04:33 -0400

    added logout to the mobile navbar

--- src/components/molecules/BottonNav.tsx
+++ src/components/molecules/BottonNav.tsx
@@ -2,8 +2,14 @@
 
 import { usePathname, useRouter } from "next/navigation";
 import { IconName, Icons } from "../atoms/icons";
+import { useAuth } from "@/context/AuthContext";
 
-const navItems: { label: string; icon: IconName; route: string }[] = [
+const navItems: {
+  label: string;
+  icon: IconName;
+  route?: string;
+  action?: () => void;
+}[] = [
   { label: "Overview", icon: "HouseIcon", route: "/dashboard" },
   {
     label: "Transactions",
@@ -22,6 +28,7 @@ const navItems: { label: string; icon: IconName; route: string }[] = [
 const BottomNav = () => {
   const pathname = usePathname();
   const router = useRouter();
+  const { logout } = useAuth();
 
   return (
     <nav className="fixed bottom-0 left-0 w-full h-[64px] bg-grey-900 flex justify-around items-end rounded-t-2xl shadow-md z-50">
@@ -31,8 +38,14 @@ const BottomNav = () => {
 
         return (
           <button
-            key={item.route}
-            onClick={() => router.push(item.route)}
+            key={item.label}
+            onClick={() => {
+              if (item.route) {
+                router.push(item.route);
+              } else if (item.action) {
+                item.action();
+              }
+            }}
             className={`flex flex-col items-center justify-center transition-colors h-[60px] w-full ${
               isActive
                 ? "text-secondary-green border-b-4 border-secondary-green rounded-t-xl bg-beige-100 pt-100"
@@ -46,6 +59,14 @@ const BottomNav = () => {
           </button>
         );
       })}
+
+      <button
+        onClick={logout}
+        className="flex flex-col items-center justify-center transition-colors h-[60px] w-full text-grey-300"
+      >
+        <Icons.WrenchIcon className="w-5 h-5" />
+        <span className="hidden sm:inline text-preset-5 font-bold">Logout</span>
+      </button>
     </nav>
   );
 };
